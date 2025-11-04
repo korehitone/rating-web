b4e76f7f71d589a430c064bc9e4dde36ea90e4a9
kore hito <aserctech@outlook.com>
2025-11-04 09:40:35 +0700

    add config template and ignore config file

---
+++ app/config/example_config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BASE_URL', 'http://localhost/rating-web/public'); 
+define('BASE_URL_IMG', 'http://localhost/rating-web/app/resource/'); 
+define('PROFILE_PATH', $_SERVER['DOCUMENT_ROOT'].'/rating-web/app/resource/profile/');
+define('MOVIE_PATH', $_SERVER['DOCUMENT_ROOT'].'/rating-web/app/resource/movie/');
+define('ACTOR_PATH', $_SERVER['DOCUMENT_ROOT'].'/rating-web/app/resource/actor/'); 
+
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
